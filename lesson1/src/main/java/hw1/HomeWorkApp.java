b15edf8e0f0d2d028de53b494e80f018818eed6d
nata <natashanv0795@gmail.com>
2022-05-18 20:39:14 +0300

    created HomeWorkApp class created method printThreeWords

---
+++ lesson1/src/main/java/hw1/HomeWorkApp.java
@@ -0,0 +1,17 @@
+package hw1;
+
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    private static void printThreeWords() {
+
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+
+}
