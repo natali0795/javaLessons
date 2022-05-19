4c82eab69a9dfa2b1dcb940469bcc39a1a7ddc85
nata <natashanv0795@gmail.com>
2022-05-19 16:09:03 +0300

    created method checkSumSign created method printColor created method compareNumbers

--- lesson1/src/main/java/hw1/HomeWorkApp.java
+++ lesson1/src/main/java/hw1/HomeWorkApp.java
@@ -4,6 +4,9 @@ public class HomeWorkApp {
 
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
     }
 
     private static void printThreeWords() {
@@ -13,5 +16,38 @@ public class HomeWorkApp {
         System.out.println("Apple");
     }
 
+    private static void checkSumSign() {
+        int a = 12;
+        int b = 13;
+        int c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
 
+    private static void printColor() {
+        int value = 1;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+   private static void compareNumbers() {
+        int a = 54;
+        int b = -42;
+        if (a>=b){
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
+        }
+
+        }
 }
+
+
+
